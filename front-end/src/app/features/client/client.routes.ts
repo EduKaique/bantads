90ab279ee608b986e13c2e342f2a4798eda4c757
Artur-Falavinha <artur.falavinha@gmail.com>
2026-03-08 21:59:32 -0300

    refactor: deixa o deposito apenas em /client/deposito

--- front-end/src/app/features/client/client.routes.ts
+++ front-end/src/app/features/client/client.routes.ts
@@ -1,17 +1,7 @@
 import { Routes } from '@angular/router';
-import { ClientDashboardPageComponent } from './pages/client-dashboard-page.component';
 import { DepositPageComponent } from './pages/deposit-page.component';
 
 export const clientRoutes: Routes = [
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'dashboard',
-  },
-  {
-    path: 'dashboard',
-    component: ClientDashboardPageComponent,
-  },
   {
     path: 'deposito',
     component: DepositPageComponent,
