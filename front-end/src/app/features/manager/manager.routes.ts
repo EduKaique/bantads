25400ab94ffba2a77d047b9b6f91bbac6df94440
pompssss <rafaelluispomponio@gmail.com>
2026-03-14 16:04:11 -0300

    [FRONTEND] [R13 - Consultar Cliente] corre

--- front-end/src/app/features/manager/manager.routes.ts
+++
@@ -1,9 +0,0 @@
-import {Routes} from '@angular/router';
-import { ConsultarClienteComponent } from './pages/013-consultar-cliente/013-consultar-cliente.component';
-
-export const managerRoutes: Routes = [
-    {
-      path: 'consultar-cliente',
-      component: ConsultarClienteComponent,
-    }
-];
