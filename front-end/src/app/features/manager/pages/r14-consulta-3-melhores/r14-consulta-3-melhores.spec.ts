fdadc2392eed761ccbfcf0eeff6e211286fc6e27
pompssss <rafaelluispomponio@gmail.com>
2026-03-22 22:36:40 -0300

    [FRONTEND] [R14 - Fix: Correções de encoding, spec test e nomenclatura em português]

--- front-end/src/app/features/manager/pages/r14-consulta-3-melhores/r14-consulta-3-melhores.spec.ts
+++ front-end/src/app/features/manager/pages/r14-consulta-3-melhores/r14-consulta-3-melhores.spec.ts
@@ -1,18 +1,18 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+﻿import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { R14Consulta3Melhores } from './r14-consulta-3-melhores';
+import { R14Consulta3MelhorComponent } from './r14-consulta-3-melhores';
 
-describe('R14Consulta3Melhores', () => {
-  let component: R14Consulta3Melhores;
-  let fixture: ComponentFixture<R14Consulta3Melhores>;
+describe('R14Consulta3MelhorComponent', () => {
+  let component: R14Consulta3MelhorComponent;
+  let fixture: ComponentFixture<R14Consulta3MelhorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [R14Consulta3Melhores]
+      imports: [R14Consulta3MelhorComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(R14Consulta3Melhores);
+    fixture = TestBed.createComponent(R14Consulta3MelhorComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
