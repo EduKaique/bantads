53afde18731a1eaf5a65cd6be9773c22285ae005
Arthur Deretti <arthurderetti05@gmail.com>
2026-04-11 16:33:23 -0300

    feat: implementação rotas e padrões do crud

--- database/init.sql
+++ database/init.sql
@@ -1,5 +1,10 @@
-CREATE DATABASE IF NOT EXISTS bantads_db;
-GRANT ALL PRIVILEGES ON bantads_db.* TO 'springuser'@'%';
-FLUSH PRIVILEGES;
+-- CREATE DATABASE IF NOT EXISTS bantads_db;
+-- GRANT ALL PRIVILEGES ON bantads_db.* TO 'springuser'@'%';
+-- FLUSH PRIVILEGES;
 
-USE bantads_db;
+-- USE bantads_db; comentado pois bantads_cliente já é criado no docker compose 
+
+CREATE SCHEMA IF NOT EXISTS ms_cliente;
+
+-- Permissões
+GRANT ALL ON SCHEMA ms_cliente TO springuser;
