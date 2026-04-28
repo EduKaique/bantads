e78e4792eb8d9079764421419080e7bb76df9c37
Anna Luiza <marianoannaluiza@gmail.com>
2026-04-28 20:45:47 -0300

    adição do UsuarioSagaConsumer

---
+++ back-end/auth/src/main/java/com/bantads/auth/UsuarioSagaConsumer.java
@@ -0,0 +1,51 @@
+package com.bantads.auth;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import com.bantads.auth.dto.AutocadastroInfoDTO;
+import com.bantads.auth.model.TipoUsuario;
+import com.bantads.auth.repository.UserRepository;
+import com.bantads.auth.security.Sha256SaltPasswordEncoder;
+import com.bantads.auth.model.User;
+
+@Component
+public class UsuarioSagaConsumer {
+    
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private Sha256SaltPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @RabbitListener(queues = "saga.autocadastro.auth")
+    public void CriarUsuarioSaga(AutocadastroInfoDTO dto) {
+        try{
+            User novoUsuario = new User();
+            novoUsuario.setNome(dto.getNome());
+            novoUsuario.setEmail(dto.getEmail());
+            novoUsuario.setSenha(passwordEncoder.encode(dto.getSenha()));
+            novoUsuario.setTipo(TipoUsuario.CLIENTE);
+            novoUsuario.setReferenciaId(dto.getCpf());
+
+            userRepository.save(novoUsuario);
+            rabbitTemplate.convertAndSend("saga.autocadastro.exchange", "saga.autocadastro.auth.ok", dto);
+
+        } catch (Exception e) {
+            rabbitTemplate.convertAndSend("saga.autocadastro.exchange", "saga.autocadastro.erro", dto);
+        }
+    }
+
+    @RabbitListener(queues = "saga.autocadastro.auth.rollback")
+    public void cancelarUsuarioSaga(AutocadastroInfoDTO dto) {
+        userRepository.findByEmail(dto.getEmail())
+            .ifPresent(user -> userRepository.delete(user));
+        System.out.println("Rollback realizado: Usuário removido do MS Auth.");
+    
+    }
+
+}
