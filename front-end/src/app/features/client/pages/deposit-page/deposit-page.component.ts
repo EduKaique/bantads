0609a6823aa62fd9cbf190c7b0eda674a811ed94
Anna Luiza <marianoannaluiza@gmail.com>
2026-05-18 21:17:58 -0300

    fix: adicao do depositPage dentro de uma pasta

--- front-end/src/app/features/client/pages/deposit-page/deposit-page.component.ts
+++ front-end/src/app/features/client/pages/deposit-page/deposit-page.component.ts
@@ -11,12 +11,12 @@ import {
 import { Router } from '@angular/router';
 import { finalize } from 'rxjs';
 
-import { AppSuccessModalComponent } from '../../../shared/components/modal-mensagem/app-success-modal';
-import { DepositRequest } from '../../../shared/models/deposit-request';
-import { formatCurrency } from '../../../shared/utils/formatters';
-import { InputPrimaryComponent } from '../../../shared/components/input-primary/input-primary.component';
-import { DepositConfirmationModalComponent } from '../components/deposit-confirmation-modal.component';
-import { ClientAccountService } from '../services/client-account.service';
+import { AppSuccessModalComponent } from '../../../../shared/components/modal-mensagem/app-success-modal';
+import { DepositRequest } from '../../../../shared/models/deposit-request';
+import { formatCurrency } from '../../../../shared/utils/formatters';
+import { InputPrimaryComponent } from '../../../../shared/components/input-primary/input-primary.component';
+import { DepositConfirmationModalComponent } from '../../components/deposit-confirmation-modal.component';
+import { ClientAccountService } from '../../services/client-account.service';
 
 const amountPattern = /^\d+(?:[.,]\d{1,2})?$/;
 
